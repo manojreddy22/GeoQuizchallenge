242bb59bcf5beac2a9a7e330783a66bbeae894be
manojred22 <manojbhpc2011@gmail.com>
2016-06-15 20:24:40 +0530

    contentDescription

--- app/src/main/java/com/example/manoj/geoquiz/Question.java
+++ app/src/main/java/com/example/manoj/geoquiz/Question.java
@@ -4,4 +4,29 @@ package com.example.manoj.geoquiz;
  * Created by manoj on 15/6/16.
  */
 public class Question {
+
+    private  int mTextResId;
+    private  boolean mAnswerTrue;
+
+
+    public Question(int testResId,boolean answerTrue){
+        mTextResId=testResId;
+        mAnswerTrue=answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
 }
